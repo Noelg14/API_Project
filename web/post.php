07f48bdb41973c6981c94c023a1c14e514034a74
Noel Griffin <griffin.noel1@gmail.com>
2021-06-04 08:21:18 +0100

    Delete post.php

--- web/post.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connect = mysql_connect(“localhost”, “noel”, “noel”); if (!connect) { die('Connection Failed: ' . mysql_error()); { mysql_select_db(“database_name”, $connect);
-
-$user_info = “INSERT INTO testperson (name, email) VALUES ('$_POST[name]', '$_POST[email]')”; if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
-
-echo “Your information was added to the database.”;
-
-mysql_close($connect); ?>
