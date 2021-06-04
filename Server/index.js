518a7018b67bd31496e4295254e7ee241ff9b8ea
Noelg14 <griffin.noel1@gmail.com>
2021-06-04 08:19:51 +0100

    First Commit

---
+++ Server/index.js
@@ -0,0 +1,214 @@
+const express=require('express');
+const cors = require('cors');
+var sqlString=require('sqlstring');
+
+var mysql = require('mysql');
+const app = express();
+const PORT =8080;
+
+app.use(express.json());
+app.use(cors(
+));
+
+app.listen(
+    PORT,
+    () => console.log(`Hello World, ${PORT}`)
+)
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: "",
+    database:""
+  })
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+
+
+
+app.get('/rest/person', (req,res) =>{ //run this when Url is called
+        var sql ='SELECT * FROM person;';
+        console.log('Ping');
+
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            res.status(200).send(
+                {
+                 result,
+            })
+          });
+
+      });
+});
+
+app.get('/rest/transaction', (req,res) =>{ //run this when Url is called
+    var sql ='SELECT * FROM transaction;';
+
+
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        res.status(200).send(
+            {
+             result,
+        })
+      });
+
+  });
+
+app.get('/rest/ping', (req,res) =>{ //run this when Url is called
+    console.log('Ping');
+    res.status(200).send({
+        message:'pong'
+
+    })
+});
+
+app.get(`/rest/person/:name`, (req,res) =>{ //run this when Url is called
+    console.log('Pong');
+    var {name} = req.params; // get from headers/param
+    console.log(req.params);
+
+  /*  if(name.includes(`'` )){
+        message="Invalid SQL";
+        res.status(500).send;
+        return null;
+    }*/
+    name= sanitize(name);
+    //console.log(name);
+
+
+        var sql =`SELECT * FROM person t where t.first_name like '%`+name+`%' or t.last_name like '%`+name+`%';`;
+
+        con.query(sql, function (err, result) {
+            if (err){
+                res.status(500).send;
+                con.end();
+                throw err
+            }
+            res.status(200).send({
+                 result,
+            })
+    });
+});
+
+
+app.post('/rest/login', (req,res) =>
+{
+        console.log(req.body);
+        var u  = req.body.user;
+        var p = req.body.password;
+        console.log(u+" "+p);
+        var key=generate().toString();
+        console.log(typeof(key));
+
+        var token={"login_token":key};
+
+
+        var sql =`SELECT * FROM person t where t.first_name like '%`+u+`%';`;
+
+        con.query(sql, function (err, result) {
+            console.log(result.length)
+
+            if(result.length === 0){
+                return res.sendStatus(500);
+            }
+            if (err){
+                res.sendStatus(500);
+                con.end();
+                throw err
+            }
+            if(u==result[0].first_name.toLowerCase() && p==result[0].password)
+            {
+            res.status(200).send({
+                token
+            })
+
+            var id=result[0].id;
+            var date=Date.now()
+
+            var sqlIn=`INSERT INTO token (token,created_at,expires,id) VALUES('`+key+`','`+date+`','`+date+`','`+id+`');`;
+
+            con.query(sqlIn, function (err, result) {
+                console.log("Successfully added token")     ;
+                if (err){
+                    throw err
+                }
+            });
+
+
+        }
+        else{
+            res.status(500).send;
+        }
+        
+    });
+
+});
+
+app.post('/rest/create',(req,res)=>{
+        var id,fname, lname,email,dob;
+
+        if(req.body.id =="" || req.body.id == null){
+            id=Date.now();
+            console.log(id);
+        }
+        else{ 
+            id=req.body.id;
+        }
+        
+        fname = req.body.fname;
+        lname = req.body.lname;
+        email= req.body.email.toString();
+        dob=req.body.dob;
+
+        var sql=`INSERT INTO person (id,first_name,last_name,email,DOB) VALUES('`+id+`','`+fname+`','`+lname+`','`+email+`','`+dob+`');`; //needs to be an insert
+
+        con.query(sql, function (err, result) {
+            if (err){
+                res.status(500).send;
+                //con.end();
+                throw err;
+            }
+
+            res.status(201).send({ })
+    });
+
+
+       // res.status(201).send({});
+});
+
+
+function sanitize(string) {
+    const map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#x27;',
+        "/": '&#x2F;',
+    };
+    const reg = /[&<>"'/]/ig;
+    return string.replace(reg, (match)=>(map[match]));
+  }
+
+function  generate(){ //might be good to array this for looping
+var num=['A','B','C','D','E','F','G','H',1,2,3,4,5,6,7,8,9,0]
+var key='';
+for(var i=0;i<=18;i++){
+
+    if(i==4 || i==9 || i ==14){
+    key+='-';
+    }
+    else{
+    
+    var o=Math.floor(Math.random() * num.length);
+    var p =num[o];
+    key+=p;
+    }
+}
+console.log(key);
+return key.toString();
+}
---
+++ web/js/root.js
@@ -0,0 +1,219 @@
+function $(v){
+    return document.getElementById(v);
+}
+var user;
+
+window.onload =function(){
+	if(window.location.href=="http://localhost/test/login.html"){
+		return null;
+	}
+	else if(document.cookie == null || document.cookie == ''){
+			// Simulate an HTTP redirect:
+			window.location.replace("http://localhost/test/login.html");
+	}
+	if(document.cookie!= null && window.location.href=="http://localhost/test/login.html" ){
+		$('logged').innerHTML='<h4 style="text-align:right; class=inline"> Logged in</h3>';
+	}
+};
+
+var clicks=0;
+
+const RAPIDAPI_API_URL = 'http://localhost:8080/rest/';
+const RAPIDAPI_REQUEST_HEADERS = {
+'Content-Type': 'application/json',
+'Access-Control-Allow-Origin': '*',
+'Access-Control-Allow-Methods': "GET, PUT, POST, DELETE, HEAD, OPTIONS"
+};
+
+	function submit(){
+		var location=document.querySelector('input[name="q"]:checked').value;
+		var url=RAPIDAPI_API_URL+location;
+
+
+		console.log(url);
+
+		//document.getElementById("text").innerHTML ='' ;
+
+		axios.get(url, { headers: RAPIDAPI_REQUEST_HEADERS })
+		.then(response => {
+          console.log(response.data);
+
+		  if(clicks >=1){
+			  var countRows= $("res").rows.length
+			  for(i=0;i< countRows;++i){
+				  console.log(countRows+ " "+i)
+					$("res").deleteRow(0);
+			  }
+			  countRows=0;
+		  }
+
+		  if(location=='transaction'){
+			for(i=0;i<response.data.result.length;i++){
+				var name =response.data.result[i].Ref;
+				//$("text").innerHTML +=name;
+				var table=$("res");
+				var row = table.insertRow(-1);
+				var cell = row.insertCell(0);
+				var cell2 =row.insertCell(1)
+				cell.innerHTML = name;
+				cell2.innerHTML= response.data.result[i].Gross;
+				clicks++;
+			}
+		  }
+		  if(location=='person'){
+			for(i=0;i<response.data.result.length;i++){
+				
+				var name =response.data.result[i].first_name+",\n";
+				//$("text").innerHTML +=name;
+
+				var table=$("res");
+				var row = table.insertRow(-1);
+				var cell = row.insertCell(0);
+				var cell2 =row.insertCell(1)
+				cell.innerHTML = name;
+				cell2.innerHTML= response.data.result[i].last_name;
+				clicks++;
+			}
+		}
+		})
+		.catch(error => {
+			console.error('', error);
+			alert("An error Ocurred! Please try again"+" "+error.status);
+		});
+	}
+
+
+	function login(){
+		var url=RAPIDAPI_API_URL+"login";
+
+		var p=$("pass").value;
+		var u=$("user").value;
+
+		if (u == "" || p=="" ||u == undefined || p==undefined ){
+			alert("Invalid username or password");
+			return null;
+		}
+
+		axios.post(url,{user:u,password:p} ,{ headers: RAPIDAPI_REQUEST_HEADERS })
+		.then(response => {
+			if(response.status != 200){
+				throw err
+			}
+          console.log(response);
+		  alert("Logged in sucessfully");
+		  var cookie=response.data.token.login_token;
+		  console.log(cookie);
+		  document.cookie="login-token="+cookie+"; site=localhost";
+
+		// Simulate an HTTP redirect:
+		window.location.replace("http://localhost/test/index.html");
+		var user=u;
+		//$('logged').innerHTML='<h3 style="text-align:right; class=inline"> Logged in as: '+user+'</h3>';
+
+		})
+		.catch(error => {
+			console.error('', error);
+			alert("An error Ocurred! Please try again");
+		});
+
+}	
+
+function search(){
+
+
+	var u=$("user").value;
+	var click=0;
+	var url=RAPIDAPI_API_URL+"person/"+u;
+
+	if (u == "" ||u == undefined ){
+		alert("invalid  name");
+		return null;
+	}
+
+	axios.get(url,{name:u} ,{ headers: RAPIDAPI_REQUEST_HEADERS })
+	.then(response => {
+	  console.log(response);
+	  //alert(response.status);
+	  document.cookie="login-token="+response.data.token+"; site=localhost";
+
+	  if(clicks >=1){
+		var countRows= $("res").rows.length
+		for(i=0;i< countRows;++i){
+			console.log(countRows+ " "+i)
+			  $("res").deleteRow(0);
+		}
+		countRows=0;
+	}
+	if(response.data.result.length==0){
+		var table=$("res");
+		var row = table.insertRow(-1);
+		var cell = row.insertCell(0);
+		cell.innerHTML = "<p>No Users Found</p>";
+		clicks++;
+	}
+
+	for(i=0;i<response.data.result.length;i++){
+				
+		var name =response.data.result[i].first_name+",\n";
+
+		var table=$("res");
+		var row = table.insertRow(-1);
+		var cell = row.insertCell(0);
+		var cell2 =row.insertCell(1);
+		var cell3 =row.insertCell(2)
+		var cell4 =row.insertCell(3)
+		var cell5 =row.insertCell(4)
+		var cell6 =row.insertCell(5)
+		var cell7 =row.insertCell(6)
+
+
+		cell.innerHTML = "<p>Name:</p>";
+		cell2.innerHTML= response.data.result[i].first_name;
+		cell3.innerHTML= response.data.result[i].last_name;
+		cell4.innerHTML="<p>| ID:</p>";
+		cell5.innerHTML=response.data.result[i].id;
+		cell6.innerHTML="<p>| Email:</p>";
+		cell7.innerHTML=response.data.result[i].email;
+
+		clicks++;
+	}
+})
+	.catch(error => {
+		console.error('', error);
+		alert("An error Ocurred! Please try again"+" "+error.status);
+	});
+
+}	
+
+function createP(){
+	url=RAPIDAPI_API_URL+"create";
+
+	var id,fname, lname,email,dob;
+	id=$("id").value
+	fname=$("fname").value;
+	lname=$("lname").value;
+	email=$("email").value;
+	dob=$("dob").value;
+
+	data={
+		"id":id,
+		"fname":fname,
+		"lname":lname,
+		"email":email,
+		"dob":dob
+}
+
+	axios.post(url,data ,{ headers: RAPIDAPI_REQUEST_HEADERS })
+		.then(response => {
+          console.log(response);
+		  //alert(response.status);
+		  alert("Success!")
+		})
+		.catch(error => {
+			console.error('', error);
+			alert("An error Ocurred! Please try again"+" "+error.status);
+		});
+}
+
+
+
